17d3b25e116727a03d0a2ca5a39efdc1646c4e75
Kevin Trang <ktrngcode@gmail.com>
2021-11-02 10:07:30 -0400

    front-end kinda done

--- bodega-cats/src/App.js
+++ bodega-cats/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [cats, setCats] = useState([])
+  const [date, setNewDate] = useState()
+  const [image, setNewImage] = useState()
+
+  const addNewImage = (event) => {
+    setNewImage(event.target.value)
+  }
+
+  const addNewDate = (event) => {
+    setNewDate(event.target.value)
+  }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/bodega-cats')
+      .then((response) => {
+        setCats(response.data)
+      })
+  })
+
+  return(
+    <main>
+      <h1>Bodega Cats</h1>
+      <section>
+        <h2>Submit New Bodega Cat</h2>
+        <form>
+          Image: <input type="text" onChange={addNewImage}/><br/><br/>
+          Date: <input type="text" onChange={addNewDate}/><br/><br/>
+          <input type="submit"/>
+        </form>
+      </section>
+      <section>
+        <h2>Cats</h2>
+        <ul>
+          {
+            cats.map((cat) => {
+              return <li>
+                <img src={cat.image} /><br/>
+                {cat.date}
+                <button onClick={(event) => {
+                  {deletedCat(cat)}
+                }}>
+                </li>
+            })
+          }
+        </ul>
+      </section>
+    </main>
+  )
 }
 
 export default App;
