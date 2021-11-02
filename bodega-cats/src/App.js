b3f221216b7b946685917f3cdf1ab92ffdbb7f31
Kevin Trang <ktrngcode@gmail.com>
2021-11-02 10:41:53 -0400

    more front end done

--- bodega-cats/src/App.js
+++ bodega-cats/src/App.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 
 const App = () => {
   const [cats, setCats] = useState([])
-  const [date, setNewDate] = useState()
-  const [image, setNewImage] = useState()
+  const [newDate, setNewDate] = useState()
+  const [newImage, setNewImage] = useState()
 
   const addNewImage = (event) => {
     setNewImage(event.target.value)
@@ -13,6 +13,49 @@ const App = () => {
 
   const addNewDate = (event) => {
     setNewDate(event.target.value)
+  }
+
+  const addNewCat = (event) => {
+    event.preventDefault()
+    axios.post(
+      'http://localhost:3000/bodega-cats',
+      {
+        image: newImage,
+        date: newDate
+      }
+    ).then(() => {
+      axios
+        .get('http://localhost:3000/bodega-cats')
+        .then((response) => {
+          setCats(response.data)
+        })
+    })
+  }
+
+  const deletedCat = (event) => {
+    axios
+      .delete(
+        `http://localhost:3000/bodega-cats/${event._id}`
+      ).then (() => {
+        axios
+          .get('http://localhost:3000/bodega-cats')
+          .then((response) => {
+            setCats(response.data)
+        })
+      })
+  }
+
+  const editCat = (cat) => {
+    axios
+      .delete(
+        `http://localhost:3000/bodega-cats/${cat._id}`
+      ).then(() => {
+        axios
+          .get('http://localhost:3000/bodega-cats')
+          .then((response) => {
+            setCats(response.data)
+        })
+      })
   }
 
   useEffect(() => {
@@ -28,7 +71,7 @@ const App = () => {
       <h1>Bodega Cats</h1>
       <section>
         <h2>Submit New Bodega Cat</h2>
-        <form>
+        <form onSubmit={addNewCat}>
           Image: <input type="text" onChange={addNewImage}/><br/><br/>
           Date: <input type="text" onChange={addNewDate}/><br/><br/>
           <input type="submit"/>
@@ -42,9 +85,12 @@ const App = () => {
               return <li>
                 <img src={cat.image} /><br/>
                 {cat.date}
-                <button onClick={(event) => {
-                  {deletedCat(cat)}
-                }}>
+                <form onSubmit={(event) => {editCat(cat)}}>
+                  Image: <input type="text" onChange={addNewImage} placeholder={cat.image}/><br/>
+                  Date: <input type="text" onChange={addNewDate} placeholder={cat.date}/><br/>
+                  <input type="submit" value="Submit Changes"/>
+                </form>
+                <button onClick={(event) => {deletedCat(cat)}}>Delete</button>
                 </li>
             })
           }
