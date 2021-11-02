c903ff3dd90b9d0393a668c33a0b2a2074eb1e16
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-11-02 10:39:18 -0400

    Created components for New Cat Form and Bodega Cats Header

--- bodega-cats/src/App.js
+++ bodega-cats/src/App.js
@@ -1,59 +1,54 @@
 import './App.css';
-import {useState, useEffect} from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const newFunction = () => 2 + 2
+import NewCatForm from './components/NewCatForm';
+import BodegaCatsHeader from './components/BodegaCatsHeader';
 
 const App = () => {
-  const [cats, setCats] = useState([])
-  const [date, setNewDate] = useState()
-  const [image, setNewImage] = useState()
+  const [cats, setCats] = useState([]);
+  const [date, setNewDate] = useState();
+  const [image, setNewImage] = useState();
 
   const addNewImage = (event) => {
-    setNewImage(event.target.value)
-  }
+    setNewImage(event.target.value);
+  };
 
   const addNewDate = (event) => {
-    setNewDate(event.target.value)
-  }
+    setNewDate(event.target.value);
+  };
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3000/bodega-cats')
-      .then((response) => {
-        setCats(response.data)
-      })
-  })
+    axios.get('http://localhost:3000/bodega-cats').then((response) => {
+      setCats(response.data);
+    });
+  });
 
-  return(
+  return (
     <main>
-      <h1>Bodega Cats</h1>
-      <section>
-        <h2>Submit New Bodega Cat</h2>
-        <form>
-          Image: <input type="text" onChange={addNewImage}/><br/><br/>
-          Date: <input type="text" onChange={addNewDate}/><br/><br/>
-          <input type="submit"/>
-        </form>
-      </section>
+      <>
+        <BodegaCatsHeader />
+      </>
+      <>
+        <NewCatForm addNewImage={addNewImage} addNewDate={addNewDate} />
+      </>
       <section>
         <h2>Cats</h2>
         <ul>
-          {
-            cats.map((cat) => {
-              return <li>
-                <img src={cat.image} /><br/>
+          {cats.map((cat) => {
+            return (
+              <li>
+                <img src={cat.image} alt="Bodega Cat" />
+                <br />
                 {cat.date}
-                <button onClick={(event) => {
-                  {deletedCat(cat)}
-                }}>
-                </li>
-            })
-          }
+                <button onClick={(event) => {}}>Delete</button>
+              </li>
+            );
+          })}
         </ul>
       </section>
     </main>
-  )
-}
+  );
+};
 
 export default App;
