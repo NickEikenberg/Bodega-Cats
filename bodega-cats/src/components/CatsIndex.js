ec96cdafee3ee05435999d687bf887d71e05ee89
Kevin Trang <ktrngcode@gmail.com>
2021-11-02 12:13:06 -0400

    trying stuff on edit form

--- bodega-cats/src/components/CatsIndex.js
+++ bodega-cats/src/components/CatsIndex.js
@@ -1,7 +1,7 @@
 const CatsIndex = (props) => {
 
   // const showEdit = () => {
-  //   let editForm = document.getElementById('edit-form')
+  //   let editForm = document.getElement('div')
   //   let editBtn = document.getElementById('edit-btn')
   //   editForm.className = 'show'
   //   editBtn.className = 'hide'
@@ -19,31 +19,33 @@ const CatsIndex = (props) => {
               <br />
               {cat.date}<br/>
 
-              <button id="edit-btn" className="show">Edit</button><br/>
+              <button id="edit-btn" className="show" >Edit</button><br/>
 
-              <form
-                id={cat._id}
-                onSubmit={(event) => {
-                  event.preventDefault()
-                  props.editCat(cat);
-                }}
-              >
-                Image:{' '}
-                <input
-                  type="text"
-                  onChange={props.addNewImage}
-                  placeholder={cat.image}
-                />
-                <br />
-                Date:{' '}
-                <input
-                  type="text"
-                  onChange={props.addNewDate}
-                  placeholder={cat.date}
-                />
-                <br />
-                <input type="submit" value="Submit Changes" />
-              </form>
+              <div className="show">
+                <form
+                  id={cat._id}
+                  onSubmit={(event) => {
+                    event.preventDefault()
+                    props.editCat(cat);
+                  }}
+                >
+                  Image:{' '}
+                  <input
+                    type="text"
+                    onChange={props.addNewImage}
+                    placeholder={cat.image}
+                  />
+                  <br />
+                  Date:{' '}
+                  <input
+                    type="text"
+                    onChange={props.addNewDate}
+                    placeholder={cat.date}
+                  />
+                  <br />
+                  <input type="submit" value="Submit Changes" />
+                </form>
+              </div>
               <button
                 onClick={(event) => {
                   props.deletedCat(cat);
