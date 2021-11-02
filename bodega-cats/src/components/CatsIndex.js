76c94441a55d9a2935d44a01fb8855d8f60bffd5
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-11-02 12:02:17 -0400

    merge

--- bodega-cats/src/components/CatsIndex.js
+++ bodega-cats/src/components/CatsIndex.js
@@ -1,15 +1,4 @@
 const CatsIndex = (props) => {
-<<<<<<< HEAD
-  // const showEdit = () => {
-  //   let editForm = document.getElementById('edit-form')
-  //   let editBtn = document.getElementById('edit-btn')
-  //   editForm.className = 'show'
-  //   editBtn.className = 'hide'
-  // }
-=======
-
->>>>>>> a6d38966e84108d725aa992d39a95ca88e4dde09
-
   return (
     <section>
       <h2>Cats</h2>
@@ -23,19 +12,13 @@ const CatsIndex = (props) => {
               {cat.date}
               <br />
 
-<<<<<<< HEAD
               <button id="edit-btn" className="show">
                 Edit
               </button>
               <br />
-=======
-
-              <button id="edit-btn" className="show">Edit</button><br/>
->>>>>>> a6d38966e84108d725aa992d39a95ca88e4dde09
 
               <form
                 id={cat._id}
-
                 onSubmit={(event) => {
                   event.preventDefault();
                   props.editCat(cat);
