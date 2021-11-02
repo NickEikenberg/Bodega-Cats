99889e924d47a70cb93371c8763c9e7f25441715
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-11-02 11:39:07 -0400

    Changes

--- bodega-cats/src/App.js
+++ bodega-cats/src/App.js
@@ -6,14 +6,11 @@ import NewCatForm from './components/NewCatForm';
 import BodegaCatsHeader from './components/BodegaCatsHeader';
 import CatsIndex from './components/CatsIndex';
 
-const newfunction = () => 2 + 4;
-
 const App = () => {
   const [cats, setCats] = useState([]);
 
   const [newDate, setNewDate] = useState();
   const [newImage, setNewImage] = useState();
-
 
   const addNewImage = (event) => {
     setNewImage(event.target.value);
@@ -22,7 +19,6 @@ const App = () => {
   const addNewDate = (event) => {
     setNewDate(event.target.value);
   };
-
 
   const addNewCat = (event) => {
     event.preventDefault();
@@ -38,7 +34,6 @@ const App = () => {
       });
   };
 
-
   const deletedCat = (event) => {
     axios.delete(`http://localhost:3000/bodega-cats/${event._id}`).then(() => {
       axios.get('http://localhost:3000/bodega-cats').then((response) => {
@@ -48,19 +43,17 @@ const App = () => {
   };
 
   const editCat = (cat) => {
-
     axios
-      .put(
-        `http://localhost:3000/bodega-cats/${cat._id}`
-      ).then(() => {
-        axios
-          .get('http://localhost:3000/bodega-cats')
-          .then((response) => {
-            setCats(response.data)
-        })
+      .put(`http://localhost:3000/bodega-cats/${cat._id}`, {
+        image: newImage || cat.image,
+        date: newDate || cat.date,
       })
-  }
-
+      .then(() => {
+        axios.get('http://localhost:3000/bodega-cats').then((response) => {
+          setCats(response.data);
+        });
+      });
+  };
 
   useEffect(() => {
     axios.get('http://localhost:3000/bodega-cats').then((response) => {
@@ -74,9 +67,12 @@ const App = () => {
         <BodegaCatsHeader />
       </>
       <>
-        <NewCatForm addNewImage={addNewImage} addNewDate={addNewDate} addNewCat={addNewCat}/>
+        <NewCatForm
+          addNewImage={addNewImage}
+          addNewDate={addNewDate}
+          addNewCat={addNewCat}
+        />
       </>
-
 
       <>
         <CatsIndex
@@ -87,7 +83,6 @@ const App = () => {
           deletedCat={deletedCat}
         />
       </>
-
     </main>
   );
 };
